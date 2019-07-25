3e279ec34a3418f436ca0747beedb492575f9dc4
raineldn <raineldn@gmail.com>
2019-07-26 00:29:56 +0100

    cleaned up code - separated register into controllers

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt');
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./controllers/register.js');
+
 const db = knex({
 	client: 'pg',
 	connection: {
@@ -42,34 +44,7 @@ app.post('/signin', (req, res) => {
 	.catch(err => res.status(400).json('wrong credentials'))
 })
 
-app.post('/register', (req, res) => {
-	const { email, name, password } = req.body;
-	const saltRounds = 10;
-	const hash = bcrypt.hashSync(password, saltRounds);
-		db.transaction(trx => {
-			trx.insert({
-				hash: hash,
-				email: email
-			})
-			.into('login')
-			.returning('email')
-			.then(loginEmail => {
-				return trx('users')
-				.returning('*')
-				.insert({
-				email: loginEmail[0],
-				name: name,
-				joined: new Date()
-				})
-				.then(user => {
-					res.json(user[0]);
-				})
-			})
-			.then(trx.commit)
-			.catch(trx.rollback)
-		})
-		.catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
